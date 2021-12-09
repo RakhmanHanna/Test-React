3e4dcf4da8416bad00692533e5f3622724e9c56b
rakhmanhanna <rakhmanhanna@gmail.com>
2021-12-09 18:47:50 +0300

    task 1 edited

--- Task1.js
+++ Task1.js
@@ -8,7 +8,7 @@
 // 5. The function should be immutable (return a new array).
 // Function example:
 // export const groupBy = (arr, key) => {}
-//I nput data:
+// Input data:
 // [
 // { id: 1, universe: "marvel", name: "Spider Man" },
 // { id: 2, universe: "marvel", name: "Iron Man" },
@@ -46,6 +46,42 @@
 
 // по любым критериям
 
+// const names = [
+//   { id: 1, universe: 'marvel', name: 'Spider Man' },
+//   { id: 2, universe: 'marvel', name: 'Iron Man' },
+//   { id: 3, universe: 'dc', name: 'Aqua Man' },
+//   { id: 4, universe: 'dc', name: 'Bat Man' },
+//   { id: 5, universe: 'marvel', name: 'Hulk' },
+// ];
+
+// Array.prototype.groupBy = function (prop) {
+//   return this.reduce(function (groups, item) {
+//     const val = item[prop];
+//     groups[val] = groups[val] || [];
+//     groups[val].push(item);
+//     return groups;
+//   }, {});
+// };
+
+// const groupByUniverse = names.groupBy('universe');
+// console.log(groupByUniverse);
+
+// const groupByName = names.groupBy('name');
+// console.log(groupByName);
+
+////
+
+const groupByNames = (arr, key) => {
+  const newGroup = {};
+  arr.forEach((item) => {
+    const newItem = Object.assign({}, item);
+    delete newItem[key];
+    newGroup[item[key]] = newGroup[item[key]] || [];
+    newGroup[item[key]].push(newItem);
+  });
+  return newGroup;
+};
+
 const names = [
   { id: 1, universe: 'marvel', name: 'Spider Man' },
   { id: 2, universe: 'marvel', name: 'Iron Man' },
@@ -54,17 +90,4 @@ const names = [
   { id: 5, universe: 'marvel', name: 'Hulk' },
 ];
 
-Array.prototype.groupBy = function (prop) {
-  return this.reduce(function (groups, item) {
-    const val = item[prop];
-    groups[val] = groups[val] || [];
-    groups[val].push(item);
-    return groups;
-  }, {});
-};
-
-const groupByUniverse = names.groupBy('universe');
-console.log(groupByUniverse);
-
-const groupByName = names.groupBy('name');
-console.log(groupByName);
+console.log(groupByNames(names, 'universe'));
